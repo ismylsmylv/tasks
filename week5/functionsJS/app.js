c920be59fdb15cb1532edc371c4876f04a9026f4
ismylsmylv <ismayilovsmyl2020@gmail.com>
2023-10-16 16:30:43 +0400

    js tasks

--- week5/functionsJS/app.js
+++ week5/functionsJS/app.js
@@ -1,16 +1,26 @@
-function getSum(num1, num2) {
-    sum = num1 + num2
-    return sum
+// function getSum(num1, num2) {
+//     sum = num1 + num2
+//     return sum
+// }
+// function getDiff(num1, num2) {
+//     diff = num1 - num2
+//     return diff
+// }
+// function result(num1, num2) {
+//     sum = getSum(num1, num2)
+//     diff = getDiff(num1, num2)
+//     result = sum * diff
+//     return result
+// }
+
+// console.log(result(9, 7));
+let obj = {
+    name: "john",
+    surname: "doe",
+    age: 32
 }
-function getDiff(num1, num2) {
-    diff = num1 - num2
-    return diff
-}
-function result(num1, num2) {
-    sum = getSum(num1, num2)
-    diff = getDiff(num1, num2)
-    result = sum * diff
-    return result
+function datas(obj) {
+    return obj.name + " " + obj.surname + " " + obj.age
 }
 
-console.log(result(9, 7));
+console.log(datas(obj));
