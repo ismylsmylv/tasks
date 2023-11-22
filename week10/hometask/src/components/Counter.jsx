01b15b315e3eedf4fc8c94191359f9f068d1e29a
ismylsmylv <ismayilovsmyl2020@gmail.com>
2023-11-22 22:23:56 +0400

    project created, creating counter

---
+++ week10/hometask/src/components/Counter.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { useReducer } from 'react'
+function()
+function Counter() {
+    return (
+        <>
+            <button>Increment</button>
+            <button>Decrement</button>
+            <p>count</p>
+
+        </>
+    )
+}
+
+export default Counter
