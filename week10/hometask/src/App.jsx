5e80fe4b5cc458e62ca0610d1318a8106e81e136
ismylsmylv <ismayilovsmyl2020@gmail.com>
2023-11-22 23:02:58 +0400

    todo completed

--- week10/hometask/src/App.jsx
+++ week10/hometask/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Counter from './components/Counter'
+import TodoList from './components/TodoList'
 function App() {
   const [count, setCount] = useState(0)
 
@@ -11,6 +12,10 @@ function App() {
       {/* count */}
       <h2>Counter</h2>
       <Counter />
+      <br />
+      {/* todo */}
+      <h2>Todo</h2>
+      <TodoList />
     </>
   )
 }
---
+++ week10/hometask/src/components/TodoList.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+function TodoList() {
+    const [input, setinput] = useState("");
+    const [list, setlist] = useState([]);
+    return (
+        <>
+            <input type="text" onChange={
+                (e) => {
+                    setinput(e.target.value)
+                    console.log(e.target.value)
+                }
+            } />
+            <button onClick={() => {
+                setinput("")
+                console.log(list)
+                console.log(input)
+                setlist([...list, input])
+
+            }}>add</button>
+            <ul>
+                {
+                    list.map(elem => {
+                        return <li>{elem}</li>
+                    })
+                }
+            </ul>
+
+        </>
+    )
+}
+
+export default TodoList
