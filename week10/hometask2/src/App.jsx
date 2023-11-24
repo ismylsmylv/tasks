8e0d7c800e131b23b1dae6b12c5673c57e15365b
ismylsmylv <ismayilovsmyl2020@gmail.com>
2023-11-24 10:47:54 +0400

    reducer task rewritten

--- week10/hometask2/src/App.jsx
+++ week10/hometask2/src/App.jsx
@@ -1,35 +1,47 @@
-import React from 'react'
-import { useState, useReducer } from 'react'
-
+import React, { useState, useReducer } from 'react'
 function Adder(state, action) {
   switch (action.type) {
     case "add":
-      return state = [...state, action.value]
+      return [...state, action.value]
+    case "delete":
+      return []
+
+    default:
+      break;
   }
 }
 function App() {
-  const [todo, settodo] = useState("");
-  const [list, dispatch] = useReducer(Adder, [])
-  return (<>
-    <input type="text" onChange={(e) => {
-      settodo(e.target.value)
+  const [todo, settodo] = useState("")
+  const [state, dispatch] = useReducer(Adder, [])
+  return (
+    <>
+      <input type="text" onChange={(e) => {
+        settodo(e.target.value)
+      }} />
+      <button onClick={() => {
+        dispatch({
+          type: "add", value: todo
+        })
+        settodo([])
+      }}>Add</button>
+      <button onClick={
+        () => {
+          dispatch({
+            type: "delete"
+          })
+        }
+      }>Delete</button>
 
 
-    }}
-      value={todo} />
-    <button onClick={() => {
-      dispatch({
-        type: "add",
-        value: todo
-      })
-    }}>Add</button>
-    <ul>
 
-      {list.map((elem, i) => {
-        return <li key={i}>{elem}</li>
-      })}
-    </ul>
-  </>
+      <ul>
+        {state.map((elem, i) => {
+          return <li key={i}>{elem}</li>
+        })}
+      </ul>
+
+
+    </>
   )
 }
 
---
+++ week10/hometask2/src/appcopy2.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { useState, useReducer } from 'react'
+
+function Adder(state, action) {
+    switch (action.type) {
+        case "add":
+            return state = [...state, action.value]
+    }
+}
+function App() {
+    const [todo, settodo] = useState("");
+    const [list, dispatch] = useReducer(Adder, [])
+    return (<>
+        <input type="text" onChange={(e) => {
+            settodo(e.target.value)
+
+
+        }}
+            value={todo} />
+        <button onClick={() => {
+            dispatch({
+                type: "add",
+                value: todo
+            })
+        }}>Add</button>
+        <ul>
+
+            {list.map((elem, i) => {
+                return <li key={i}>{elem}</li>
+            })}
+        </ul>
+    </>
+    )
+}
+
+export default App
