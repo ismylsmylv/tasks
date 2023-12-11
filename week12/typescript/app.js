68e39a9e1dfe6c069c0a15d0c66c885f9e8c71c9
ismylsmylv <ismayilovsmyl2020@gmail.com>
2023-12-11 22:16:34 +0400

    typescript practised

---
+++ week12/typescript/app.js
@@ -0,0 +1,5 @@
+var message = "Hello W02930239209w";
+console.log(message);
+var heading = document.createElement("h1");
+heading.textContent = message;
+document.body.appendChild(heading);
---
+++ week12/typescript/app.ts
@@ -0,0 +1,5 @@
+let message: string = "Hello W02930239209w";
+console.log(message);
+let heading = document.createElement("h1");
+heading.textContent = message;
+document.body.appendChild(heading);
