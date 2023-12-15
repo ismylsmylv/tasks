81a43617ee18a88ec8b8613a341c06be5f582ae7
ismylsmylv <ismayilovsmyl2020@gmail.com>
2023-12-15 11:25:11 +0400

    todo task completed

--- week12/reactTypeTodo/src/App.tsx
+++ week12/reactTypeTodo/src/App.tsx
@@ -1,26 +1,59 @@
-import { Dispatch } from "@reduxjs/toolkit";
+import { RootState } from "@reduxjs/toolkit/query";
+import { Dispatch, SetStateAction, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
-import { addTodo } from "./redux/slice/todoSlice";
-import { useSelector, useDispatch } from "react-redux";
-import { SetStateAction } from "react";
+import { Todo, addTodo, getTodo, deleteTodo } from "./redux/slice/todoSlice";
+interface Props {
+  todo: Todo;
+  todos: Todo[];
+  addTodo: Dispatch<SetStateAction<Todo>>;
+  getTodo: Dispatch<SetStateAction<Todo[]>>;
+}
 function App() {
-  const addTodo = useSelector((state) => state.todo.addTodo);
+  // const addTodo = useSelector((state) => state.todo.addTodo);
   const dispatch = useDispatch();
-
-  interface Props {
-    todo: Todo;
-    todos: Todo[];
-    addTodo: Dispatch<SetStateAction>;
-  }
+  const todo = useSelector((state: RootState) => state.todo.todo);
+  const todos = useSelector((state: RootState) => state.todo.todos);
+  const [input, setinput] = useState("");
   return (
     <>
-      <button
-        onClick={() => {
-          dispatch(addTodo());
-        }}
-      >
-        click
-      </button>
+      <div className="input" style={{ display: "flex", flexDirection: "row" }}>
+        <input
+          type="text"
+          value={input}
+          onChange={(e) => {
+            dispatch(getTodo(e.target.value));
+            setinput(e.target.value);
+          }}
+        />
+        <button
+          onClick={() => {
+            dispatch(addTodo(todo));
+            setinput("");
+          }}
+        >
+          click
+        </button>
+      </div>
+
+      <div className="listing">
+        <ul>
+          {todos.map((elem: Todo) => {
+            return (
+              <li key={elem.id}>
+                {elem.title}{" "}
+                <button
+                  onClick={() => {
+                    dispatch(deleteTodo(elem.id));
+                  }}
+                >
+                  delete
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </>
   );
 }
--- week12/reactTypeTodo/src/redux/slice/todoSlice.ts
+++ week12/reactTypeTodo/src/redux/slice/todoSlice.ts
@@ -1,6 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
 import type { PayloadAction } from "@reduxjs/toolkit";
-
+import { v4 as uuidv4 } from "uuid";
 export interface Todo {
   id: string;
   title: string;
@@ -19,11 +19,25 @@ export const todoSlice = createSlice({
   name: "todo",
   initialState,
   reducers: {
-    addTodo: (state, action: PayloadAction<string>) => {
-      console.log("object");
+    getTodo: (state, action: PayloadAction<string>) => {
+      const obj: Todo = {
+        id: uuidv4(),
+        title: action.payload,
+      };
+      state.todo = obj;
+    },
+    addTodo: (state, action: PayloadAction<Todo>) => {
+      state.todos.push(action.payload);
+      console.log(state.todos);
+    },
+    listTodo: (state, action: PayloadAction<string>) => {
+      state.todos = [...state.todos, state.todo];
+    },
+    deleteTodo: (state, action: PayloadAction<string>) => {
+      state.todos = state.todos.filter((elem) => elem.id != action.payload);
     },
   },
 });
 
-export const { addTodo } = todoSlice.actions;
+export const { getTodo, addTodo, listTodo, deleteTodo } = todoSlice.actions;
 export default todoSlice.reducer;
