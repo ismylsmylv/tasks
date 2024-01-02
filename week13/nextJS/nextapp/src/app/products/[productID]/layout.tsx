3897ba5523ccbbabeb4cc8c5292e1064dc603c1e
ismylsmylv <ismayilovsmyl2020@gmail.com>
2024-01-02 11:13:22 +0400

    metadata added

---
+++ week13/nextJS/nextapp/src/app/products/[productID]/layout.tsx
@@ -0,0 +1,8 @@
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      {children}
+      <h2>Featured Prods</h2>
+    </>
+  );
+}
--- week13/nextJS/nextapp/src/app/products/[productID]/page.tsx
+++ week13/nextJS/nextapp/src/app/products/[productID]/page.tsx
@@ -1,7 +1,22 @@
-export default function ProductDetials({
-  params,
-}: {
-  params: { productID: string };
-}) {
+import { Metadata } from "next";
+
+export const generateMetadata = async ({ params }: Props): Metadata => {
+  const title = await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(`order ${params.productID}`);
+    }, 100);
+  });
+  return {
+    title: `product ${title}`,
+  };
+};
+
+type Props = {
+  params: {
+    productID: string;
+  };
+};
+
+export default function ProductDetials({ params }: Props) {
   return <h1>Prod details {params.productID}</h1>;
 }
