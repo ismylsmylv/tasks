91a44dd515269e5c9513330230db7af4796e6792
ismylsmylv <ismayilovsmyl2020@gmail.com>
2024-01-02 10:46:02 +0400

    route group added

--- week13/nextJS/nextapp/src/app/page.tsx
+++ week13/nextJS/nextapp/src/app/page.tsx
@@ -1,3 +1,10 @@
+import Clicker from "./components/button";
+
 export default function Home() {
-  return <h1>Home page</h1>;
+  return (
+    <>
+      <h1>Home page</h1>
+      {/* <button onClick={Clicker}>To profile</button> */}
+    </>
+  );
 }
