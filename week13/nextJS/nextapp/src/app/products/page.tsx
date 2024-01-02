fc543c4775fc2b3ef8973f8598c714bf822ee44c
ismylsmylv <ismayilovsmyl2020@gmail.com>
2024-01-02 14:09:09 +0400

    navigation

--- week13/nextJS/nextapp/src/app/products/page.tsx
+++ week13/nextJS/nextapp/src/app/products/page.tsx
@@ -1,11 +1,24 @@
+import Link from "next/link";
 export default function Products() {
+  const prodID = 100;
   return (
     <>
       <h1>Product list</h1>
       <ul>
-        <li>Product 1</li>
-        <li>Product 2</li>
-        <li>Product 3</li>
+        <li>
+          <Link href={"/products/1"}>Product 1</Link>
+        </li>
+        <li>
+          <Link href={"/products/2"} replace>
+            Product 2
+          </Link>
+        </li>
+        <li>
+          <Link href={"/products/3"}>Product 3</Link>
+        </li>
+        <li>
+          <Link href={`/products/${prodID}`}>Product {prodID}</Link>
+        </li>
       </ul>
     </>
   );
