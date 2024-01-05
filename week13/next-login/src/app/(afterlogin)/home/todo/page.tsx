db65dfb2827b7662401e943058af0b858ac14729
ismylsmylv <ismayilovsmyl2020@gmail.com>
2024-01-05 09:56:11 +0400

    todo completed

--- week13/next-login/src/app/(afterlogin)/home/todo/page.tsx
+++ week13/next-login/src/app/(afterlogin)/home/todo/page.tsx
@@ -11,34 +11,89 @@ import {
   editer,
 } from "@/redux/slices/counterSlice";
 import { useSelector } from "react-redux";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import Checkbox from "@mui/material/Checkbox";
+import IconButton from "@mui/material/IconButton";
+import CommentIcon from "@mui/icons-material/Comment";
+import "./style.css";
 export default function Todo() {
   const todos = useSelector((state: any) => state.counter.todos);
   let todo = useSelector((state: any) => state.counter.todo);
   console.log();
   // const [todo, settodo] = useState("");
   const dispatch = useDispatch();
+  const [checked, setChecked] = React.useState([0]);
+
   return (
-    <div>
-      <input
-        type="text"
-        placeholder="Add todo"
-        value={todo}
-        onChange={(e) => {
-          dispatch(getter(e.target.value));
-          // settodo(e.target.value);
-        }}
-      />
-      <button
-        onClick={() => {
-          // console.log(todo);
-          dispatch(adder(todo));
-          // settodo("");
-          dispatch(empty());
-        }}
-      >
-        Add
-      </button>
-      <ul>
+    <div className="todoCard">
+      <div className="inpField">
+        <input
+          type="text"
+          placeholder="Add todo"
+          value={todo}
+          onChange={(e) => {
+            dispatch(getter(e.target.value));
+            // settodo(e.target.value);
+          }}
+        />
+        <button
+          onClick={() => {
+            // console.log(todo);
+            dispatch(adder(todo));
+            // settodo("");
+            dispatch(empty());
+          }}
+        >
+          Add
+        </button>
+      </div>
+      <List sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
+        {todos &&
+          todos.map((elem: any) => {
+            return (
+              <ListItem
+                key={uuidv4()}
+                secondaryAction={
+                  <IconButton
+                    edge="end"
+                    aria-label="comments"
+                    onClick={() => {
+                      let editTodo = prompt("Edit todo");
+                      dispatch(editer([elem, editTodo]));
+                    }}
+                  >
+                    <CommentIcon />
+                  </IconButton>
+                }
+                disablePadding
+              >
+                <ListItemButton
+                  role={undefined}
+                  onClick={() => {
+                    dispatch(deleter(elem));
+                  }}
+                  dense
+                >
+                  <ListItemIcon>
+                    <Checkbox
+                      edge="start"
+                      // checked={checked.indexOf(value) !== -1}
+                      tabIndex={-1}
+                      disableRipple
+                      // inputProps={{ "aria-labelledby": labelId }}
+                    />
+                  </ListItemIcon>
+                  <ListItemText id={uuidv4()} primary={elem} />
+                </ListItemButton>
+              </ListItem>
+            );
+          })}
+      </List>
+      {/* <ul>
         {todos &&
           todos.map((elem: any) => {
             return (
@@ -62,7 +117,7 @@ export default function Todo() {
               </li>
             );
           })}
-      </ul>
+      </ul> */}
     </div>
   );
 }
